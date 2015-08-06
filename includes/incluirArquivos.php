6069abdd06986d4deb326642a2aa8312f070c4a9
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-08-06 18:09:04 -0300

    Inclusão de arquivos JS adicionada

--- includes/incluirArquivos.php
+++ includes/incluirArquivos.php
@@ -4,4 +4,12 @@
 	foreach(scandir($diretorioJSLib) as $c => $v)
 		if($v != "." and $v != "..")
 			$incluirArquivos .= '<script type="text/javascript" src="'.$diretorioJSLib.$v.'"></script>';
+	$diretorioJSArquivos = "js/arquivos/";
+	foreach(scandir($diretorioJSArquivos) as $c => $v)
+		if($v != "." and $v != "..")
+			$incluirArquivos .= '<script type="text/javascript" src="'.$diretorioJSArquivos.$v.'"></script>';
+	$diretorioArquivos = "includes/arquivos/";
+	foreach(scandir($diretorioArquivos) as $c => $v)
+		if($v != "." and $v != "..")
+			include($diretorioArquivos.$v);
 ?>
