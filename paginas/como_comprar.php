677ccbea4f63e93c281861d2f73d2a880e2a83f0
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-08-06 18:08:36 -0300

    Página 'Como Comprar' criada

---
+++ paginas/como_comprar.php
@@ -0,0 +1,99 @@
+<?php
+	$titulo = "Como Comprar";
+	$conteudo = '
+		<b>Formas de compra:</b><br>
+		Você pode comprar diretamente da fábrica em São Paulo, pessoalmente,
+		via telefone, e-mail, ou fax.<br>
+		<br>
+		<b>Fomas de pagamento:<br>
+		<br>
+		A prazo</b>:<br>
+		em 3 vezes para compras entre R$ 400,00 e R$ 1.000,00<br>
+		em 4 vezes para compras entre R$ 1.001,00 e R$ 2.000,00<br>
+		em 5 vezes para compras acima de R$ 2.000,00<br>
+		<ul>
+			<li>
+				Cartão de crédito Visa ou Master Card **
+				<span class="textoPequeno">(pessoalmente ou a distância)</span>;
+			</li>
+		</ul>                
+		<b>À vista:<br>
+		Para pagamento à vista o desconto é de 5%.</b><br>
+		<span class="textoPequeno">(somente para os itens de nossa fabricação)</span>
+		<ul>
+			<li>
+				Depósito em conta corrente*
+			</li>
+			<li>
+				Cartão de crédito Visa ou Master Card **
+				<span class="textoPequeno">(pessoalmente ou a distância)</span>
+			</li>
+		</ul>                
+		<span class="textoPequeno">*Após efetuar o depósito é necessário
+		enviar a cópia do comprovante <b>com o número do seu telefone de
+		contato</b>, via e-mail (<a href="mailto:atek@atek.com.br" class="textoPequeno">atek@atek.com.br</a>)
+		ou fax: (11) 3272-0366/3209-3131.<br>
+		**O pagamento com cartão de crédito VISA ou MASTER poderá ser
+		efetuado em até 5 vezes à distância (dependendo do valor - vide acima).
+		Para compras à distância via telefone com cartão de crédito, são exigidos
+		cópias do cartão de crédito, CPF e RG frente e verso, via fax.<br>                    
+		<br>
+		<b>IMPORTANTE:</b> Para a sua segurança não recomendamos enviar imagens
+		destes documentos via e-mail.</span><br>
+		<br>
+		<b>Para efetuar a compra é necessário enviar os seguintes dados, para
+		emissão de nota fiscal e despacho da mercadoria:</b>
+		<ul>
+			<li>
+				Nome (pessoa física) ou Razão Social (pessoa jurídica) completo
+			</li>
+			<li>
+				Telefone para contato
+			</li>
+			<li>
+				Endereço completo
+			</li>
+			<li>
+				Bairro
+			</li>
+			<li>
+				CEP
+			</li>
+			<li>
+				Cidade
+			</li>
+			<li>
+				Estado
+			</li>
+			<li>
+				CPF ou CNPJ
+			</li>
+			<li>
+				Data de Nascimento (somente pessoa física)
+			</li>
+			<li>
+				RG ou Inscrição Estadual
+			</li>
+			<li>
+				Transportadora pela qual tem preferência (se preferir podemos indicar uma)
+			</li>
+		</ul>
+		<b>Frete:</b><br>
+		A remessa segue via transportadora, seja de sua escolha ou, caso prefira,
+		podemos indicar uma.<br>
+		O preço do frete varia de acordo com o local do destino, tamanho e peso
+		da mercadoria, número de volumes e valor da mesma.<br>
+		Frete por conta do cliente. O frete deverá ser pago pelo cliente
+		diretamente para a transportadora no ato da entrega (não trabalhamos
+		com frete pago, exceto correios).<br>
+		Objetos pequenos poderão ser enviados pelo SEDEX.<br>
+		Lembramos que é fundamental que exista alguém no endereço especificado
+		para recebimento da mercadoria.<br>
+		Informamos que não é possível agendar horário de entrega.<br>
+		<br>
+		<b>Horário de atendimento:</b><br>                  
+		De segunda a sexta-feira das 09:00 às 18:00 (exceto feriados)<br>
+		Aos sábados das 09:00 às 13:00, exceto aqueles que antecedam feriados
+		na segunda-feira e posteriores a feriados na sexta-feira.<br>
+	';
+?>
