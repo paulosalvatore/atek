ac52f51056ca30763eb1a8f2b04def74a320763d
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-08-06 18:08:19 -0300

    Página 'Localização' criada

---
+++ paginas/localizacao.php
@@ -0,0 +1,30 @@
+<?php
+	$titulo = "Localização";
+	$conteudo = '
+		<b>Nosso endereço:</b><br>
+		Rua Galvão Bueno, 825 - Liberdade<br>
+		01506-000 - São Paulo - SP <br>
+		<br>
+		<b>Fone/Fax:</b> (11) 3272-0366/(11) 3209-3131<br>
+		<b>Celulares:</b><br>
+		<b>VIVO</b> - (11) 95770-5218<br>
+		<b>TIM</b> - (11) 98376-1890<br>
+		<b>OI</b> - (11) 95416-0938<br>
+		<b>CLARO</b> – (11) 99896-8761<br>
+		<br>
+		<b>Horário de atendimento:</b><br>
+		De segunda a sexta-feira das 09:00 às 18:00 (exceto feriados)<br>
+		Aos sábados das 09:00 às 13:00, exceto aqueles que antecedam feriados
+		na segunda-feira e posteriores a feriados na sexta-feira.<br>
+		<iframe marginheight="0" marginwidth="0" src="http://maps.google.com.br/maps?f=q&amp;source=s_q&amp;hl=pt-BR&amp;geocode=&amp;q=Rua+Galv%C3%A3o+Bueno,+825&amp;aq=&amp;sll=-14.239424,-53.186502&amp;sspn=50.448321,93.076172&amp;ie=UTF8&amp;hq=&amp;hnear=R.+Galv%C3%A3o+Bueno,+825+-+Liberdade,+S%C3%A3o+Paulo,+01506-000&amp;ll=-23.562925,-46.634603&amp;spn=0.015735,0.025706&amp;z=15&amp;iwloc=A&amp;output=embed" frameborder="0" height="400" scrolling="no" width="600"></iframe><br>
+		<small><a href="http://maps.google.com.br/maps?f=q&amp;source=embed&amp;hl=pt-BR&amp;geocode=&amp;q=Rua+Galv%C3%A3o+Bueno,+825&amp;aq=&amp;sll=-14.239424,-53.186502&amp;sspn=50.448321,93.076172&amp;ie=UTF8&amp;hq=&amp;hnear=R.+Galv%C3%A3o+Bueno,+825+-+Liberdade,+S%C3%A3o+Paulo,+01506-000&amp;ll=-23.562925,-46.634603&amp;spn=0.015735,0.025706&amp;z=15&amp;iwloc=A" target="_blank" class="texto_azul">Exibir mapa ampliado</a></small><br>
+		<br>
+		<img src="imagens/conteudo/circuloAzul.gif" align="absmiddle"> <b>Convênio estacionamento 1 - <i>Spring Park</i></b> - primeiro edifício ao lado esquerdo da Galvão Bueno - (Unicsul) - quase na esquina (número 862)<br>
+		<img src="imagens/conteudo/circuloAzul.gif" align="absmiddle"> <b>Convênio estacionamento 2</b> - Rua Galvão Bueno, 848 – ao lado esquerdo<br>
+		<img src="imagens/conteudo/circuloAzul.gif" align="absmiddle"> <b>Convênio estacionamento 3 - <i>Pare Aki</i></b> - Rua Galvão Bueno, 733 – ao lado direito<br>
+		<br>
+		Estacionamento gratuito pelo período de uma hora.<br>
+		<br>
+		<img src="imagens/conteudo/estacionamento.jpg">
+	';
+?>
