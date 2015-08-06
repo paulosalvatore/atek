274350065bb84b9457ecb92f8cf2d83842df19b7
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-08-06 18:09:17 -0300

    Classe de 'Produtos' criada

--- includes/classes/produtos.php
+++ includes/classes/produtos.php
@@ -1,4 +1,19 @@
 <?php
 	class Produtos {
+		public function pegarUrl($id){
+			return "?p=produtos-$id-".$this->pegarDescricao($id, true);
+		}
+		public function pegarDescricao($id, $limparAcentuacao){
+			$informacoes = $this->pegarInformacoes($id);
+			$Funcao = new Funcao();
+			return (count($informacoes) > 0 ? $Funcao->limparString($informacoes["descricao_site"]) : "Não Encontrado");
+		}
+		public function pegarInformacoes($id){
+			$informacoes = array();
+			$queryProduto = mysql_query("SELECT * FROM produtos WHERE (id LIKE '$id')");
+			while($resultadoProduto = mysql_fetch_assoc($queryProduto))
+				$informacoes = $resultadoProduto;
+			return $informacoes;
+		}
 	}
 ?>
