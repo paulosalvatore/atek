1f79e4e25570110a7e5bac8eeb84f4654f234fee
paulosalvatore <paulo.salvatore@hotmail.com.br>
2015-08-06 18:08:03 -0300

    Pequena modificação no arquivo 'index.php'

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-	header("Content-Type: text/html; charset=ISO-8859-1", true);
 	require_once("conexao/conexao.php");
 	session_start();
 	include("includes/iniciarClasses.php");
+	header("Content-Type: text/html; charset=ISO-8859-1", true);
 	include("estrutura.php");
 ?>
