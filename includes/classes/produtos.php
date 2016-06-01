55de5aa26e50b4293e6307d3f8bb2dcdcca0b0f7
paulosalvatore <paulo.salvatore@hotmail.com.br>
2016-06-01 11:51:26 -0300

    Otimização da Classe 'Produtos'

--- includes/classes/produtos.php
+++ includes/classes/produtos.php
@@ -4,16 +4,20 @@
 			$Anuncios = new Anuncios();
 			return $Anuncios->pegarUrl($anuncio, array($descricao));
 		}
+
 		public function pegarDescricao($id){
 			$informacoes = $this->pegarInformacoes($id);
 			return (count($informacoes) > 0 ? $informacoes["descricao"] : "Não Encontrado");
 		}
+
 		public function pegarInformacoes($id){
 			$informacoes = array();
 			$Funcao = new Funcao();
+
 			$queryProdutos = mysql_query("SELECT * FROM `produtos` ".$Funcao->carregarSearchSQL("id", is_array($id) ? $id : array($id)));
 			while($resultadoProdutos = mysql_fetch_assoc($queryProdutos))
 				$informacoes[$resultadoProdutos["id"]] = $resultadoProdutos;
+
 			if(count($informacoes) > 0){
 				$queryAnuncios = mysql_query("SELECT * FROM `anuncios_produtos` ".$Funcao->carregarSearchSQL("produto", array_keys($informacoes)));
 				while($resultadoAnuncios = mysql_fetch_assoc($queryAnuncios)){
@@ -22,6 +26,7 @@
 					$informacoes[$produtoId]["link"] = $this->pegarUrl($resultadoAnuncios, array("descricao" => $descricao));
 				}
 			}
+
 			return (count($informacoes) == 1 ? $informacoes[$id] : $informacoes);
 		}
 	}
